50d6335ab677e30fb4aed9df4783933bd54027d5
Diego Garrido Alvial <dgarridoal@gmail.com>
2025-05-10 20:42:52 -0400

    feat: update color palette

--- lua/one-hunter-material/init.lua
+++ lua/one-hunter-material/init.lua
@@ -1,37 +1,46 @@
 local M = {}
 
--- Color Palette
+-- Color Palette (One Hunter Material)
 local colors = {
-	bg = "#1D2126",
-	fg = "#E3E1E1",
-	bg_highlight = "#2B2E31",
-	bg_selection = "#3D4043",
-	line_numbers = "#46494D",
-	comment = "#7F8286",
-	cursor_line = "#2B2E31",
-	border = "#3D4043",
-	red = "#f06292",
-	green = "#66dfc4",
-	yellow = "#f7bc62",
-	blue = "#64b5f6",
-	cyan = "#4fc3f7",
-	magenta = "#d88ee4",
-	orange = "#f4a343",
-	red_dim = "#bb1b3f",
-	green_dim = "#176e61",
-	blue_dim = "#05699f",
-	orange_dim = "#bc5412",
-	error = "#f06292",
-	warning = "#f4a343",
-	info = "#64b5f6",
-	hint = "#66dfc4",
+	bg = "#1D2126", -- editor.background
+	fg = "#E3E1E1", -- editor.foreground
+	bg_highlight = "#2B2E31", -- line highlight, inactive selection background
+	bg_selection = "#E3E1E1" .. "33", -- selection background (alpha 20%)
+	hover_bg = "#3D4043", -- hover highlight background
+	find_bg = "#bc5412", -- find match background
+	find_bg_dim = "#bc5412cc", -- find match highlight background
+	range_highlight = "#46494D", -- range highlight background
+
+	line_numbers = "#46494D", -- editorLineNumber.foreground
+	comment = "#7F8286", -- comments, inactive foreground
+	cursor_line = "#2B2E31", -- editor.lineHighlightBackground
+	border = "#3D4043", -- borders (FloatBorder, peekView border, etc.)
+
+	red = "#f06292", -- errors, keywords
+	green = "#66dfc4", -- strings, diff adds
+	yellow = "#f7bc62", -- numbers, warnings
+	blue = "#64b5f6", -- operators, types
+	cyan = "#4fc3f7", -- functions, hints
+	magenta = "#d88ee4", -- decorators
+	orange = "#f4a343", -- warnings, accents
+
+	red_dim = "#bb1b3f99", -- removed text background dim
+	green_dim = "#176e6199", -- inserted text background dim
+	blue_dim = "#05699f", -- incoming merge content background
+	orange_dim = "#bc5412", -- diff change header
+
+	error = "#f06292", -- DiagnosticError
+	warning = "#f4a343", -- DiagnosticWarn
+	info = "#64b5f6", -- DiagnosticInfo
+	hint = "#66dfc4", -- DiagnosticHint
 	none = "NONE",
 }
 
 local function setup_highlights()
-	local highlights = {
+	local hl = {
+		-- UI Basics
 		Normal = { fg = colors.fg, bg = colors.bg },
-		NormalFloat = { fg = colors.fg, bg = colors.bg_highlight },
+		NormalFloat = { fg = colors.fg, bg = colors.cursor_line },
 		NormalNC = { fg = colors.fg, bg = colors.bg },
 		Cursor = { fg = colors.bg, bg = colors.fg },
 		CursorLine = { bg = colors.cursor_line },
@@ -41,8 +50,12 @@ local function setup_highlights()
 		ColorColumn = { bg = colors.bg_highlight },
 		Conceal = { fg = colors.comment },
 		CursorColumn = { bg = colors.cursor_line },
+
+		-- Search & Find
 		IncSearch = { fg = colors.bg, bg = colors.yellow },
 		Search = { fg = colors.bg, bg = colors.yellow },
+
+		-- UI elements
 		VertSplit = { fg = colors.border },
 		FloatBorder = { fg = colors.border, bg = colors.bg_highlight },
 		TabLine = { fg = colors.comment, bg = colors.bg_highlight },
@@ -54,16 +67,16 @@ local function setup_highlights()
 		Folded = { fg = colors.comment, bg = colors.bg_highlight },
 		FoldColumn = { fg = colors.comment },
 		Pmenu = { fg = colors.fg, bg = colors.bg_highlight },
-		PmenuSel = { fg = colors.fg, bg = colors.bg_selection },
+		PmenuSel = { fg = colors.fg, bg = colors.hover_bg },
 		PmenuSbar = { bg = colors.bg_highlight },
 		PmenuThumb = { bg = colors.border },
-		WildMenu = { fg = colors.fg, bg = colors.bg_selection },
-		Visual = { bg = colors.bg_selection },
-		VisualNOS = { bg = colors.bg_selection },
+		WildMenu = { fg = colors.fg, bg = colors.hover_bg },
+		Visual = { bg = colors.hover_bg },
+		VisualNOS = { bg = colors.hover_bg },
+
+		-- Diagnostics
 		ErrorMsg = { fg = colors.error },
 		WarningMsg = { fg = colors.warning },
-		MoreMsg = { fg = colors.green },
-		Question = { fg = colors.green },
 		DiagnosticError = { fg = colors.error },
 		DiagnosticWarn = { fg = colors.warning },
 		DiagnosticInfo = { fg = colors.info },
@@ -72,14 +85,20 @@ local function setup_highlights()
 		DiagnosticUnderlineWarn = { sp = colors.warning, undercurl = true },
 		DiagnosticUnderlineInfo = { sp = colors.info, undercurl = true },
 		DiagnosticUnderlineHint = { sp = colors.hint, undercurl = true },
+
+		-- Diff
 		DiffAdd = { bg = colors.green_dim },
 		DiffChange = { bg = colors.blue_dim },
 		DiffDelete = { bg = colors.red_dim },
 		DiffText = { bg = colors.blue },
+
+		-- Spell
 		SpellBad = { sp = colors.error, undercurl = true },
 		SpellCap = { sp = colors.warning, undercurl = true },
 		SpellRare = { sp = colors.info, undercurl = true },
 		SpellLocal = { sp = colors.info, undercurl = true },
+
+		-- Syntax
 		Comment = { fg = colors.comment, italic = true },
 		Constant = { fg = colors.fg },
 		String = { fg = colors.green },
@@ -98,9 +117,7 @@ local function setup_highlights()
 		Exception = { fg = colors.cyan },
 		PreProc = { fg = colors.cyan },
 		Include = { fg = colors.red },
-		Define = { fg = colors.red },
 		Macro = { fg = colors.blue },
-		PreCondit = { fg = colors.cyan },
 		Type = { fg = colors.blue },
 		StorageClass = { fg = colors.red },
 		Structure = { fg = colors.yellow },
@@ -110,33 +127,30 @@ local function setup_highlights()
 		Tag = { fg = colors.red },
 		Delimiter = { fg = colors.fg },
 		SpecialComment = { fg = colors.comment, italic = true },
-		Debug = { fg = colors.red },
 		Underlined = { underline = true },
 		Ignore = { fg = colors.comment },
 		Error = { fg = colors.error },
 		Todo = { fg = colors.yellow, bold = true },
+
+		-- Treesitter
 		["@comment"] = { link = "Comment" },
 		["@error"] = { link = "Error" },
 		["@none"] = { bg = colors.none, fg = colors.none },
 		["@preproc"] = { link = "PreProc" },
-		["@define"] = { link = "Define" },
+		["@define"] = { link = "Macro" },
 		["@operator"] = { link = "Operator" },
 		["@punctuation.delimiter"] = { fg = colors.fg },
 		["@punctuation.bracket"] = { fg = colors.fg },
 		["@punctuation.special"] = { fg = colors.fg },
 		["@string"] = { link = "String" },
 		["@string.regex"] = { fg = colors.orange },
-		["@string.escape"] = { fg = colors.fg },
-		["@string.special"] = { fg = colors.fg },
 		["@character"] = { link = "Character" },
-		["@character.special"] = { fg = colors.fg },
 		["@boolean"] = { link = "Boolean" },
 		["@number"] = { link = "Number" },
 		["@float"] = { link = "Float" },
 		["@function"] = { link = "Function" },
 		["@function.builtin"] = { fg = colors.cyan },
 		["@function.call"] = { fg = colors.cyan },
-		["@function.macro"] = { fg = colors.blue },
 		["@method"] = { fg = colors.cyan },
 		["@method.call"] = { fg = colors.cyan },
 		["@constructor"] = { fg = colors.yellow },
@@ -174,23 +188,16 @@ local function setup_highlights()
 		["@text.todo.unchecked"] = { fg = colors.red },
 		["@text.diff.add"] = { fg = colors.green },
 		["@text.diff.remove"] = { fg = colors.red },
-		["@text.comment"] = { link = "Comment" },
 	}
 
-	for group, style in pairs(highlights) do
+	for group, style in pairs(hl) do
 		vim.api.nvim_set_hl(0, group, style)
 	end
 end
 
 function M.setup()
-	-- Activate termguicolors
 	vim.o.termguicolors = true
-	vim.g.background = "dark"
-
-	-- Clear all default highlights
 	vim.cmd("hi clear")
-
-	-- Set highlights
 	setup_highlights()
 end
 
